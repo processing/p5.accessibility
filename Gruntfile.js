cd1715bee70f920fb1e9e22373fe6098df0194a0
Luis Morales-Navarro <luis.mor.nav@gmail.com>
2018-06-08 11:02:09 -0600

    change library name

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,16 +13,16 @@ module.exports = function(grunt) {
         // the files to concatenate
         src: ['src/colorNamer.js','src/createHTMLstructure.js' ,'src/_data.js','src/registry.js','src/interceptorHelperFunctions.js','src/baseInterceptor.js', 'src/entities/_baseEntity.js', 'src/entities/backgroundEntity.js', 'src/entities/fillEntity.js', 'src/entities/shapeEntity.js', 'src/entities/textEntity.js', 'src/ntc.min.js' , 'src/textInterceptor/*.js','src/gridInterceptor/*.js','src/soundInterceptor/*.js'],
         // the location of the resulting JS file
-        dest: 'dist/p5-accessibility.js'
+        dest: 'dist/p5.accessibility.js'
       }
     },
     uglify: {
       options: {
-        banner: '/*! p5-accessibility */\n'
+        banner: '/*! p5.accessibility */\n'
       },
       dist: {
         files: {
-          'dist/p5-accessibility.min.js': ['<%= concat.dist.dest %>']
+          'dist/p5.accessibility.min.js': ['<%= concat.dist.dest %>']
         }
       }
     },
