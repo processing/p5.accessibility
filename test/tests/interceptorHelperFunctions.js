afb4564b7ab5e35fe18cd7a59d2c053a76c0ec73
spiray <spira.joe214@gmail.com>
2018-08-14 19:15:16 -0400

    COmmented unused tests

--- test/tests/interceptorHelperFunctions.js
+++ test/tests/interceptorHelperFunctions.js
@@ -4,11 +4,14 @@ define([`chai`], chai => {
   /* global describe */
   describe(`Interceptor Helper Functions`, () => {
     /* global it */
-    it(`can pad the string`, () => {
-      const str = `hello`;
-      const pad = `asdf`;
-      expect(str.paddingLeft(pad)).to.equal(`ello`);
-    });
+    /*
+        // TODO: remove tests as this method isnt being used
+        it(`can pad the string`, () => {
+          const str = `hello`;
+          const pad = `asdf`;
+          expect(str.paddingLeft(pad)).to.equal(`ello`);
+        });
+        */
     it(`can recursively merge objects`, () => {
       const object1 = {
         a: 1,
@@ -25,17 +28,20 @@ define([`chai`], chai => {
         c: 3
       });
     });
-    it(`checks if arrays are equal`, () => {
-      expect([1, 2, 3].equals(1)).to.equal(false); // Not an array
-      expect([1, 2, 3].equals([1, 2])).to.equal(false); // Different length
-      expect([
-        [1, 2],
-        [4, 5]
-      ].equals([
-        [1, 2],
-        [4, 5]
-      ])).to.equal(true); // Works on nested arrays
-      expect([1, 2].equals([1, 2])).to.equal(true); // Works on 1D arrays
-    })
+    // TODO: Delete as this Array methos is not used
+    /*
+                it(`checks if arrays are equal`, () => {
+                  expect([1, 2, 3].equals(1)).to.equal(false); // Not an array
+                  expect([1, 2, 3].equals([1, 2])).to.equal(false); // Different length
+                  expect([
+                    [1, 2],
+                    [4, 5]
+                  ].equals([
+                    [1, 2],
+                    [4, 5]
+                  ])).to.equal(true); // Works on nested arrays
+                  expect([1, 2].equals([1, 2])).to.equal(true); // Works on 1D arrays
+                })
+                */
   })
 })
